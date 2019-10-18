460e8789cfc2d82f047003f39ba47caf2d716d98
Stefanie Davis <davisstefanie2010@gmail.com>
2019-10-18 18:13:33 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -5,7 +5,7 @@ class Shoe
   BRANDS = []
 
   def initialize(brand)
-    @brand = brand
+    self.brand = brand
   end
 
   def cobble
@@ -13,9 +13,9 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
   
-  # def brand=(brand)
-  #   @brand = brand
-  #   BRANDS << brand
+  def brand=(brand)
+    @brand = brand
+    BRANDS << brand
   end 
   
 end
